2c543a77b3df491a71b67aaa624ad3e2cdec7600
OgurtsovDmitry <defaultikom@gmail.com>
2023-01-13 16:17:54 +0200

    relationships

--- Viewer/Viewer/Models/Reviewer.cs
+++ Viewer/Viewer/Models/Reviewer.cs
@@ -1,11 +1,9 @@
 ﻿namespace Viewer.Models
 {
-    public class Book
+    public class Reviewer
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public Author Author { get; set; }
-        public Genre Genre { get; set; }
         public ICollection<Review> Reviews { get; set; }
     }
 }
