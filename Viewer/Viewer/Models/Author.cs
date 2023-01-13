b5a330ca70f30506a550b1d3e5a132cb38375761
OgurtsovDmitry <defaultikom@gmail.com>
2023-01-13 17:41:59 +0200

    seed

--- Viewer/Viewer/Models/Author.cs
+++ Viewer/Viewer/Models/Author.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<Book> Books { get; set; }
-        public ICollection<Genre> Genres { get; set; }
+        public ICollection<Genre> Genres { get; set; }        
     }
 }
--- Viewer/Viewer/Seed.cs
+++ Viewer/Viewer/Seed.cs
@@ -1,10 +1,41 @@
-﻿namespace Viewer
+﻿using System.Diagnostics.Metrics;
+using Viewer.Data;
+using Viewer.Models;
+
+namespace Viewer
 {
     public class Seed
     {
-        internal void SeedDataContext()
+        private readonly DataContext dataContext;
+        public Seed(DataContext context)
         {
-            throw new NotImplementedException();
+            this.dataContext = context;
+        }
+        public void SeedDataContext()
+        {
+            if (!dataContext.Authors.Any())
+            {
+                new Author()
+                {
+                    //    Book = new Book()
+                    //    {
+                    //        Name = "Consolidation",
+                    //        Genre = new Genre()
+                    //        {
+                    //            Name = "Fantastic"
+                    //        },
+                    //        Reviews = new List<Review>()
+                    //            {
+                    //                new Review { Title="Fantastic book",Text = "Some text",
+                    //                Reviewer = new Reviewer(){ Name = "Some Author"} }
+                    //            }
+                    //    },
+
+                };
+                    
+            }             
+            dataContext.SaveChanges();
+           
         }
     }
 }
